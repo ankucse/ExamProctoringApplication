946ede57c3e882ebdc8711fb5c43a413eedd7e75
ankucse <49396582+ankucse@users.noreply.github.com>
2026-04-27 23:45:37 +0530

    initialize Exam Proctoring Application with Gradle setup and basic structure

---
+++ src/main/java/com/ExamProctoringApplication/ExamProctoringApplication/ExamProctoringApplication.java
@@ -0,0 +1,13 @@
+package com.ExamProctoringApplication.ExamProctoringApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamProctoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamProctoringApplication.class, args);
+	}
+
+}
